5174aab18179de0b3199b0a24a5b44186c3708b5
Christos Malliaridis <c.malliaridis@gmail.com>
2020-10-21 18:14:28 +0200

    Add samples app

---
+++ sample-app/src/main/java/com/google/android/youtube/samples/MainActivity.java
@@ -0,0 +1,14 @@
+package com.google.android.youtube.samples;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
